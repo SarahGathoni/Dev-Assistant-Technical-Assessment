5f23b3b05eb8792a4c17e81a8103442023041709
SarahGathoni <sarah.wanjiku.gathoni@gmail.com>
2023-02-11 06:42:06 -0800

    problem2

--- problem#2.py
+++ problem#2.py
@@ -1,17 +1,23 @@
-#a function that returns the maximum distance between a feld and water tower
-def get_distance(field , tower):
-    a = len(field)
-    b = len(tower)
+#a fuction that returns the maximum distance between a feld and a towe
+def get_distance(fields, towers):
 
-    x = 0
-    y = 0
-    
+    a = len(fields)
+    b = len(towers)
+
+    i, j = 0, 0
+
     distance = 0
 
-    while x < a and y < b:
-        distance = max(distance, abs(field[x] - tower[y]))
-        if field[x] < tower[y]:
-            x += 1
-        else;
-        j += 1
+    while i < a and j < b:
+
+        distance = max(distance, abs(fields[i] - towers[j]))
+
+        if fields[i] < towers[j]:
+
+            i += 1
+
+        else:
+
+            j += 1
+
     return distance
